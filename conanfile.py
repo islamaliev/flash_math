ed7ac68240c72d7f145fdaf52f7a58aeaad9e669
Islam Aliev <aliev.islam@gmail.com>
2018-05-21 16:11:18 +0200

    small correction to packaging

--- conanfile.py
+++ conanfile.py
@@ -8,21 +8,13 @@ class FlashMathConan(ConanFile):
     #default_options = ""
     exports_sources = "*"
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
-        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
-        #cmake.configure(source_dir="include")
         cmake.build()
 
     def package(self):
-        self.copy("*.h", "include", "include")
+        self.copy("*.h", "include", "src")
         self.copy("*.lib", "lib", "", keep_path=False)
         self.copy("*.a", "lib", "", keep_path=False)
 
-    def package_info(self):
-        self.cpp_info.libs = ["flash_math"]
-    
