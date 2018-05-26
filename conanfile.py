07f36bd83252ffc1265866279f3e58771f766e1d
Islam Aliev <aliev.islam@gmail.com>
2018-05-26 20:56:21 +0200

    ready to be used by parent project

--- conanfile.py
+++ conanfile.py
@@ -18,3 +18,14 @@ class FlashMathConan(ConanFile):
         self.copy("*.lib", "lib", "", keep_path=False)
         self.copy("*.a", "lib", "", keep_path=False)
 
+    def package_info(self):
+        libs = ['flash_math']
+
+        self.cpp_info.libs = []
+        for lib in libs:
+            if self.settings.os == "Windows" and self.settings.build_type == "Debug":
+                suffix = "d"
+            else:
+                suffix = ""
+            self.cpp_info.libs += ["%s%s" % (lib, suffix)]
+
